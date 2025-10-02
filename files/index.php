7beda72046322a9eee324cf11c8151437d6d4a4b
Ubuntu <ubuntu@ip-172-31-27-184.ec2.internal>
2025-10-02 17:01:15 +0000

    first commit

---
+++ files/index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head><title>Server IP</title></head>
+<body>
+    <h1>Your Public IP is:
+    <?php
+        // First try IMDSv2 (token-based)
+        $token = @shell_exec("curl -s -X PUT 'http://169.254.169.254/latest/api/token' -H 'X-aws-ec2-metadata-token-ttl-seconds: 21600'");
+        
+        if ($token) {
+            // Use the token to get the public IP
+            $publicIp = @shell_exec("curl -s -H 'X-aws-ec2-metadata-token: $token' http://169.254.169.254/latest/meta-data/public-ipv4");
+        } else {
+            // Fallback to IMDSv1
+            $publicIp = @shell_exec("curl -s http://169.254.169.254/latest/meta-data/public-ipv4");
+        }
+
+        echo $publicIp ? $publicIp : "Unavailable";
+    ?>
+    </h1>
+</body>
+</html>
+
+
